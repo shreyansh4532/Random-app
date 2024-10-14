1cfccc433207349e95c7fed0bed2011d1e8ae800
Shreyansh Mishra <shreyanshmishracr7@gmail.com>
2024-10-14 12:39:56 +0530

    Added welcome msg

--- src/main/java/com/shreyansh/demo_1/Demo1Application.java
+++ src/main/java/com/shreyansh/demo_1/Demo1Application.java
@@ -1,5 +1,6 @@
 package com.shreyansh.demo_1;
 
+import foo.bar.WelcomeMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
+
+		WelcomeMessage w = new WelcomeMessage();
+		System.out.print(w.getMsg());
 	}
 
 }
---
+++ src/main/java/foo/bar/WelcomeMessage.java
@@ -0,0 +1,9 @@
+package foo.bar;
+
+public class WelcomeMessage {
+
+    public String getMsg() {
+        return "Welcome!!!";
+    }
+
+}
